959dfee77c39818cf8a550f80f81586a94fe8b44
yuta <y.takak1234@gmail.com>
2026-09-06 06:38:49 +0900

    changed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from diffusers import StableDiffusionPipeline
 import torch
 from translate import Translator
 import os
+import argparse
 
 model_file = 'v1-5-pruned-emaonly.safetensors'
 
@@ -9,13 +10,18 @@ if not os.path.exists(model_file):
     url = 'https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors'
     torch.hub.download_url_to_file(url, model_file, hash_prefix=None, progress=True)
 
-device = "cpu"
+if torch.cuda.is_available():
+    device = 'cuda'
+elif torch.backends.mps.is_available():
+    device = 'mps'
+else:
+    device = 'cpu'
 
 pipe = StableDiffusionPipeline.from_single_file(model_file).to(device)
 generator = torch.Generator(device)
 honyaku = Translator('en','ja').translate
 
 while True:
-    prompt = input("Enter your prompt: ")
+    prompt = input("プロンプトを入力してください: ")
     img = pipe(honyaku(prompt), width = 256, height = 256, num_inference_steps=10, generator=generator).images[0]
     img.save('output.jpg')
--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @echo off
 if not exist venv (python -m venv venv)
 call venv\Scripts\activate.bat
-pip install diffusers["torch"] torchvision transformers translate
+pip install diffusers transformers translate
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu132
 python main.py
