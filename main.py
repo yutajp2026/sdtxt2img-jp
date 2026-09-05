de80e4604a415e6ada075105cce06d1b0fafc23c
yuta <y.takak1234@gmail.com>
2026-09-06 08:51:53 +0900

    changed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from diffusers import StableDiffusionPipeline
 import torch
 from translate import Translator
 import os
+import argparse
 
 model_file = 'v1-5-pruned-emaonly.safetensors'
 
@@ -9,13 +10,11 @@ if not os.path.exists(model_file):
     url = 'https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors'
     torch.hub.download_url_to_file(url, model_file, hash_prefix=None, progress=True)
 
-if torch.cuda.is_available():
-    device = 'cuda'
-elif torch.backends.mps.is_available():
-    device = 'mps'
-else:
-    device = 'cpu'
+parser = argparse.ArgumentParser(description='説明')
+parser.add_argument('--device', type=str, default='cpu', help='実行デバイス (例: cpu, cuda)')
+args = parser.parse_args()
 
+device = args.device
 pipe = StableDiffusionPipeline.from_single_file(model_file).to(device)
 honyaku = Translator('en','ja').translate
 
