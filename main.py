0a86eac0b055f6682480fc875cf67e91af682431
yuta <y.takak1234@gmail.com>
2026-09-05 22:53:38 +0900

    changed

--- main.py
+++ main.py
@@ -9,12 +9,7 @@ if not os.path.exists(model_file):
     url = 'https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors'
     torch.hub.download_url_to_file(url, model_file, hash_prefix=None, progress=True)
 
-if(torch.cuda.is_available()):
-    device = 'cuda'
-elif(torch.backends.mps.is_available()):
-    device = 'mps'
-else:
-    device = 'cpu'
+device = "cpu"
 
 pipe = StableDiffusionPipeline.from_single_file(model_file).to(device)
 generator = torch.Generator(device)
